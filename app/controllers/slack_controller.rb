6653e941bde4ebbe742ed18c889cba85759d066a
maxathome <maxim.levine@gmail.com>
2024-05-03 12:39:18 -0400

    add logging for failed api response

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -12,9 +12,29 @@ class SlackController < ApplicationController
   end
 
   private
-
   def get_eth_price
-    response = HTTParty.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-    response.parsed_response['ethereum']['usd']
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'
+    response = HTTParty.get(url)
+  
+    # Log the response for debugging
+    Rails.logger.info("API Response: #{response}")
+  
+    if response.success?
+      ethereum_price = response.parsed_response['ethereum']['usd']
+      Rails.logger.info("Ethereum Price: #{ethereum_price}")
+      return ethereum_price
+    else
+      # Log error or notify about the failure
+      Rails.logger.error("Failed to retrieve Ethereum price from API")
+      return nil # Or handle this situation appropriately
+    end
+  rescue => e
+    Rails.logger.error("Exception occurred: #{e.message}")
+    nil
   end
+  
+#   def get_eth_price
+#     response = HTTParty.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+#     response.parsed_response['ethereum']['usd']
+#   end
 end
