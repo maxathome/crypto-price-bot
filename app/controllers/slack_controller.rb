8d53042a9cf6fb15e9429aaa47344d9c65c40182
maxathome <maxim.levine@gmail.com>
2024-05-03 12:26:18 -0400

    adding private method back

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,19 +1,20 @@
 class SlackController < ApplicationController
-    skip_before_action :verify_authenticity_token
-  
-    def commands
-      case params[:text].strip.downcase
-      when 'eth'
-        price = get_eth_price
-        render json: { text: "The current price of Ethereum is $#{price}" }
-      else
-        render json: { text: "Unsupported command" }
-      end
-    end
-  
-    def get_eth_price
-      response = HTTParty.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-      response.parsed_response['ethereum']['usd']
+  skip_before_action :verify_authenticity_token
+
+  def commands
+    case params[:text].strip.downcase
+    when 'eth'
+      price = get_eth_price
+      render json: { text: "The current price of Ethereum is $#{price}" }
+    else
+      render json: { text: "Unsupported command" }
     end
   end
-  
+
+  private
+
+  def get_eth_price
+    response = HTTParty.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+    response.parsed_response['ethereum']['usd']
+  end
+end
